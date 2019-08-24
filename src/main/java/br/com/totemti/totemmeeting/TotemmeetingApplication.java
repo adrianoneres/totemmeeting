6d4761326859f2d6ae0dede7a798de24e12b9762
Adriano Neres <ah.driano@gmail.com>
2019-08-24 09:17:18 -0400

    first commit

---
+++ src/main/java/br/com/totemti/totemmeeting/TotemmeetingApplication.java
@@ -0,0 +1,13 @@
+package br.com.totemti.totemmeeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TotemmeetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TotemmeetingApplication.class, args);
+	}
+
+}
